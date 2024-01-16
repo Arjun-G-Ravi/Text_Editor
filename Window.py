7cfc59aa98ef96912b48141989012315868f73ee
Arjun <arjungravi1@gmail.com>
2024-01-16 21:25:58 +0530

    BAd turn back

--- Window.py
+++ Window.py
@@ -4,13 +4,14 @@ import tkinter as tk
 def text_editor():
     window = tk.Tk()
     window.title('Text Editor')
-    window.geometry("400x300")
-
     txt_edit = tk.Text(window)
-    txt_edit.grid(row=0, column=1, sticky='nsew')
+    txt_edit.grid(row=0, column=2)
 
     window.mainloop()
 
 
 if __name__ == '__main__':
     text_editor()
+
+
+# This is doing everything for me. We have to go deeper!
