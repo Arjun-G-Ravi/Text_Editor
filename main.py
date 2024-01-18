7a3d38c53b637231986b689af2cd247e143f8c96
Arjun <arjungravi1@gmail.com>
2024-01-18 15:57:57 +0530

    Works

--- main.py
+++ main.py
@@ -1,41 +1,42 @@
-from multiprocessing import Process
+from multiprocessing import Process, Manager
 import os
 import time
 import keyboard
 
-global shared_queue
-shared_queue = []
-
-def read_keyboard():
-    global shared_queue
-    def on_key_event(e):
-        global shared_queue
+def read_keyboard(shared_queue):
+    def on_key_event(e, shared_queue):
         if e.event_type == 'up':
+        
             shared_queue.append(e.name)
-            print("Shared q 1:", shared_queue)
+            # print("Shared q 1:", shared_queue)
         return e
 
-    keyboard.hook(on_key_event)
+    keyboard.hook(lambda e: on_key_event(e, shared_queue))
     keyboard.wait('esc')
         
-def editor():
-    global shared_queue
+def editor(shared_queue):
     out = ''
-    print(shared_queue)
     while out != 'esc':
         time.sleep(0.1)
-        print("Shared q 2:", shared_queue)
+        # print("Shared q 2:", shared_queue)
         if shared_queue:
-            print("BOAT ------------------------")
+            # print("BOAT ------------------------")
             out = shared_queue.pop(0)
-            print("Popped from shared_queue")      
+            print(f'Popped "{out}" from shared_queue')      
 
 
-pKeyRead = Process(target=read_keyboard)
-pRunEditor = Process(target=editor)
 
-pRunEditor.start()
-pKeyRead.start()
 
-pRunEditor.join()
-pKeyRead.join()
+
+if __name__ == '__main__':
+    with Manager() as manager:
+        shared_queue = manager.list()
+
+        pKeyRead = Process(target=read_keyboard, args=(shared_queue,))
+        pRunEditor = Process(target=editor, args=(shared_queue,))
+
+        pRunEditor.start()
+        pKeyRead.start()
+
+        pRunEditor.join()
+        pKeyRead.join()
