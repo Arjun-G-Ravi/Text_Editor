555a5ac3ed9476074ea3f79f9432fc30cc949095
Arjun <arjungravi1@gmail.com>
2024-01-24 18:12:59 +0530

    Added all arrow keys

--- main.py
+++ main.py
@@ -7,14 +7,35 @@ from multiprocessing import Process, Manager
 import tkinter as tk
 
 
+# class TextEditor:
+#     def __init__(self, root, shared_data):
+#         self.root = root
+#         self.root.title("COW - Text Editor")
+#         self.root.geometry("600x800")
+#         self.shared_data = shared_data
+#         self.listbox = tk.Listbox(root)
+#         self.listbox.pack(pady=10, fill=tk.BOTH, expand=True)
+#         self.update_data()
+
+
 class TextEditor:
     def __init__(self, root, shared_data):
         self.root = root
         self.root.title("COW - Text Editor")
         self.root.geometry("600x800")
         self.shared_data = shared_data
-        self.listbox = tk.Listbox(root)
+
+        vertical_scrollbar = tk.Scrollbar(root, orient=tk.VERTICAL)
+        horizontal_scrollbar = tk.Scrollbar(root, orient=tk.HORIZONTAL)
+        self.listbox = tk.Listbox(
+            root, yscrollcommand=vertical_scrollbar.set, xscrollcommand=horizontal_scrollbar.set)
         self.listbox.pack(pady=10, fill=tk.BOTH, expand=True)
+
+        vertical_scrollbar.config(command=self.listbox.yview)
+        horizontal_scrollbar.config(command=self.listbox.xview)
+        vertical_scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+        horizontal_scrollbar.pack(side=tk.BOTTOM, fill=tk.X)
+
         self.update_data()
 
     def update_data(self):  # to the GUI
@@ -29,7 +50,8 @@ class TextEditor:
 
 def read_keyboard(shared_queue, shared_data):
     def on_key_event(e, shared_queue):
-        if e.event_type == 'up' or e.name == 'esc':
+        global shift
+        if e.event_type == 'down' or e.name == 'esc':
             shared_queue.append(e.name)
         return e
 
@@ -49,7 +71,6 @@ def editor(shared_queue, shared_data):
             character = shared_queue.pop(0)
             # Make changes to out here
             hmap = {'space': ' '}
-            print("Char: ", character, r, c)
             if character == 'enter':
                 shared_data.append(manager.list())
                 r += 1
@@ -58,7 +79,7 @@ def editor(shared_queue, shared_data):
                 if r == 0 and c == 0:
                     # Beginning
                     pass
-                elif c == 0 :
+                elif c == 0:
                     # one row up
                     r -= 1
                     c = len(shared_data[r])
@@ -66,11 +87,11 @@ def editor(shared_queue, shared_data):
                     # Remove character to left
                     shared_data[r].pop(c-1)
                     c -= 1
-                    
+
             elif character == 'left':
                 if r == 0 and c == 0:
                     pass
-                elif c == 0 :
+                elif c == 0:
                     print('Row up')
                     # one row up
                     r -= 1
@@ -79,11 +100,24 @@ def editor(shared_queue, shared_data):
                     # print("Delete char")
                     c -= 1
 
+            elif character == 'right':
+                if c < len(shared_data[r]):
+                    c += 1
+            
+            elif character == 'up':
+                if r>0:
+                    r-=1
+                    c = min(c, len(shared_data[r]))
+            elif character == 'down':
+                if r<len(shared_data)-1:
+                    r+=1
+                    c = min(c, len(shared_data[r]))
             else:
                 if character in hmap:
                     character = hmap[character]
                 shared_data[r].insert(c, character)
                 c += 1
+            print("Char:", character, ' CursorIndex:', r, c)
 
 
 def run_window(shared_data, shared_queue):
